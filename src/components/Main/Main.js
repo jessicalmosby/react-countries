7acd2e006139d9f10f9ace981205860cf2318259
Jessica Mosby <jessicamosby@mac.com>
2023-01-18 20:58:43 -0500

    select drop down is working, can filter through continents

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useCountries } from '../../hooks/useCountries.js';
 import FlagCard from '../FlagCard/FlagCard.js';
 import './Main.css';
 
 export default function Main() {
   const countries = useCountries();
+  const [continent, setContinents] = useState('all');
+  const continents = [...new Set(countries.map(({ continent }) => continent))];
+
+  const filtered = countries.filter(
+    (country) => country.continent === continent || continent === 'all'
+  );
 
   return (
     <main>
-      {countries.map((country) => (
+      <select onChange={(e) => setContinents(e.target.value)}>
+        <option value="all">All</option>
+        {continents.map((continent) => (
+          <option key={continent} value={continent}>
+            {continent}
+          </option>
+        ))}
+      </select>
+      {filtered.map((country) => (
         <FlagCard key={country.name} {...country} />
       ))}
     </main>
